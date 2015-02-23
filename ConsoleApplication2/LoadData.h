a010e34066f24c4f9d427683082c81a24e539c6e
Dylsmith <dilsmith@ucdavis.edu>
2015-02-23 09:47:43 -0800

    Missing includes

---
+++ ConsoleApplication2/LoadData.h
@@ -0,0 +1,12 @@
+#ifndef LOADDATA_H
+#define LOADDATA_H
+
+#include "stdafx.h"
+
+double distanceBetween2(int origin, int destination);
+void parseClosePoints();
+void parsePeople();
+void parseTours();
+void parseTrips();
+
+#endif
